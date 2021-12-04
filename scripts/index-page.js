482c5f3c5176a88229e91edccd2fe4649a2cf187
Duncan Macdonald <dunc.macdonald@gmail.com>
2021-12-04 00:20:40 -0800

    index page mobile and tablet views finished

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,10 +1,10 @@
 window.onload = () => {
+  //preexisting comments in date order
   const comments = [
     {
-      name: "Connor Walton",
-      comment:
-        "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-      timestamp: new Date(2021, 01, 17),
+      name: "Miles Acosta",
+      comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+      timestamp: new Date(2020, 11, 20),
     },
     {
       name: "Emilie Beach",
@@ -13,9 +13,10 @@ window.onload = () => {
       timestamp: new Date(2021, 0, 9),
     },
     {
-      name: "Miles Acosta",
-      comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-      timestamp: new Date(2020, 11, 20),
+      name: "Connor Walton",
+      comment:
+        "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+      timestamp: new Date(2021, 01, 17),
     },
   ];
 
@@ -36,22 +37,43 @@ window.onload = () => {
     const commentContainer = document.getElementById('conversation__comments');
     const card = document.createElement('div');
     card.setAttribute("class", "card");
-    commentContainer.appendChild(card);
+    commentContainer.insertBefore(card, document.querySelector('.card'));
 
+    if("avatar" in com) {
+      const user_image = document.createElement('img');
+      user_image.setAttribute("alt", "user_avatar");
+      user_image.setAttribute("class", "avatar");
+      user_image.setAttribute("src", com.avatar);
+      card.appendChild(user_image);
+    } else{ //if no image in object make a div that can be a grey circle without a missing file icon
+      const user_image = document.createElement('div');
+      user_image.setAttribute("alt", "user_avatar");
+      user_image.setAttribute("class", "avatar");
+      card.appendChild(user_image);
+    }
+
+    //container for right side card content
+    const rightOfCard = document.createElement('div');
+    rightOfCard.setAttribute("class", "card__right");
+    card.appendChild(rightOfCard);
+
+    //flex container for name and time
     const topOfCard = document.createElement('div');
-    card.appendChild(topOfCard);
+    rightOfCard.appendChild(topOfCard);
 
+    //name
     const name = document.createElement('h4');
     name.innerText = com.name;
     topOfCard.appendChild(name);
 
+    //time
     const time = document.createElement('div');
     time.innerText = month + "/" + date + "/" + year;
     topOfCard.appendChild(time);
 
     const message = document.createElement('p');
     message.innerText = com.comment;
-    card.appendChild(message);
+    rightOfCard.appendChild(message);
   } 
 
 
@@ -59,13 +81,11 @@ window.onload = () => {
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     const aTimestamp = new Date();
-    //const date = aTimestamp.getDate();
-    comments.push({name: e.target.user_name.value, comment: e.target.message.value, timestamp: aTimestamp});
-    //console.log(e.target.message.value);
-    console.log(comments);
+    comments.push({name: e.target.user_name.value, comment: e.target.message.value, timestamp: aTimestamp, avatar: "./assets/images/Mohan-muruge.jpg"});
     
     displayComment(comments[comments.length - 1]);
     form.reset();
+    
   });
 
 
